00ef54f3809695db5ad9a9e4ae3857430a3bb20f
Vlad Aleksieiev <vladyslav.aleksieiev@gmail.com>
2020-08-11 12:03:49 +0300

    feature/setup-database

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { connect } from 'mongoose';
+import { getLogger } from 'log4js';
 
+import { AppModule } from './app.module';
 import { PORT } from './constants/env';
+import { CONFIG } from './constants/database';
+
+const logger = getLogger();
+logger.level = 'debug';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await connect(
+    `mongodb+srv://${CONFIG.mongoName}:${CONFIG.mongoPassword}@cluster.zo5oj.mongodb.net/${CONFIG.mongoDatabase}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  logger.debug('database successfully connected');
 
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    logger.debug(`listening on port ${PORT}`);
+  });
 }
 bootstrap();
